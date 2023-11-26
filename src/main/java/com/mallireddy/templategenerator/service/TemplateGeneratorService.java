b1c8d22f00c71e18fe4a8951a095cbc607d535b1
Mallikarjuna Reddy Nimmakayala <mallikarjunareddy.nimmakayala@gmail.com>
2023-11-26 23:45:12 +0530

    changed template ref

--- src/main/java/com/mallireddy/templategenerator/service/TemplateGeneratorService.java
+++ src/main/java/com/mallireddy/templategenerator/service/TemplateGeneratorService.java
@@ -24,7 +24,7 @@ import java.util.regex.Pattern;
 public class TemplateGeneratorService {
 
     private static final String DESTINATION_PATH = "src/main/resources/temp/projects";
-    private static final String TEMPLATE_PATH = "-vcs-ref template-restructure https://github.com/MallikarjunaReddyN/copier-springboot.git ";
+    private static final String TEMPLATE_PATH = "-vcs-ref main https://github.com/MallikarjunaReddyN/copier-springboot.git ";
 
     public Resource generateProject(Map<String, String> inputData) {
         validateInputData(inputData);
