38ac00d4fe06d24b84aaf82fcfee68c550f2bd2a
Mallikarjuna Reddy Nimmakayala <mallikarjunareddy.nimmakayala@gmail.com>
2023-12-01 12:55:27 +0530

    removed unused imports

--- src/main/java/com/mallireddy/templategenerator/config/WebMvcConfig.java
+++ src/main/java/com/mallireddy/templategenerator/config/WebMvcConfig.java
@@ -1,7 +1,6 @@
 package com.mallireddy.templategenerator.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
